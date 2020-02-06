68e9353351201bba8845252806be479d0ed13d50
alexandreerick <erickalexandrex@gmail.com>
2020-02-06 11:53:10 -0300

    finalizacao do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,27 +1,13 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-	const { github_username, techs } = req.body;
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-	const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+routes.get('/search', SearchController.index);
 
-	const { name = login, bio, avatar_url } = apiResponse.data;
-
-	const techsArray = techs.split(',').map(tech => tech.trim());
-
-	const dev = await Dev.create({
-		github_username,
-		name,
-		avatar_url,
-		bio,
-		techs: techsArray
-	})
-	
-	return res.json(dev);
-});
 
 module.exports = routes;
